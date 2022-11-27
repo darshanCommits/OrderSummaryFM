29419bb3190928d50dbdaa6732af00956767004c
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2022-11-27 16:39:53 +0530

    html structure ready

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      paleBlue: "hsl(225, 100%, 94%)",
+      brightBlue: "hsl(245, 75%, 52%)",
+      veryPaleBlue: "hsl(225, 100%, 98%)",
+      desaturatedBlue: "hsl(224, 23%, 55%)",
+      darkBlue: "hsl(223, 47%, 23%)",
+    },
+    extend: {
+      fontFamily: {
+        serif: ["Red Hat Display"]
+      },
+    },
+  },
+  plugins: [],
+};
